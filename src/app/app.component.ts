7adde3e87d3bceb007a02e7af6da93d7ffbd7318
kabirules <javifont@gmail.com>
2018-04-06 23:44:11 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  location = {};
+  lat:number = 0;
+  lon:number = 0;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    Observable.interval(5000).subscribe(x => {
+      this.updateLocation();
+    });    
+  }
+
+  updateLocation() {
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(position => {
+        this.location = position.coords;
+        this.lat = position.coords.latitude;
+        this.lon = position.coords.longitude;
+      });
+   }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
 
 import { AppComponent } from './app.component';
+
+
 
 
 @NgModule({
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxMapboxGLModule.forRoot({
+      accessToken: 'pk..oYdvq_XT79XIefBeuDNFVg'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
