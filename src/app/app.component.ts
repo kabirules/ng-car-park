d80e17333f4502b0a663fc2df836e3655c725378
kabirules <javifont@gmail.com>
2018-04-07 23:11:26 +0200

    added parking marker and save current position locally

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,14 +11,25 @@ export class AppComponent implements OnInit {
   location = {};
   lat:number = 0;
   lon:number = 0;
+  renderMarker:boolean=false;
+
+  PARK_LAT:string = 'parkLAT';
+  PARK_LON:string = 'parkLON';
 
   constructor() {
   }
 
   ngOnInit() {
+    // Check current location every 5s
     Observable.interval(5000).subscribe(x => {
       this.updateLocation();
-    });    
+    });
+    //Check if we already parked the car before
+    this.lat = +localStorage.getItem(this.PARK_LAT);
+    this.lon = +localStorage.getItem(this.PARK_LON);
+    if (this.lat!=null && this.lon!=null) {
+      this.renderMarker = true;
+    }
   }
 
   updateLocation() {
@@ -29,5 +40,13 @@ export class AppComponent implements OnInit {
         this.lon = position.coords.longitude;
       });
    }
+  }
+
+  park() {
+    //save the current position in localStorage
+    localStorage.setItem(this.PARK_LAT, this.lat.toString());
+    localStorage.setItem(this.PARK_LON, this.lon.toString());
+    //make sure the marker is rendered
+    this.renderMarker=true;
   }
 }
