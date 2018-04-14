14f75fa5e46b33ed8d0d2dd68b286e0a4c4e74d9
kabirules <javifont@gmail.com>
2018-04-14 13:26:31 +0200

    Define map dynamically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
+import * as mapboxgl from  'mapbox-gl';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+/*
+glmapbox docs
+https://github.com/Wykks/ngx-mapbox-gl/wiki/API-Documentation
+How to set access token
+https://stackoverflow.com/questions/44332290/mapbox-gl-typing-wont-allow-accesstoken-assignment#answer-44393954
+Tutorial (events)
+https://angularfirebase.com/lessons/build-realtime-maps-in-angular-with-mapbox-gl/
+*/
 export class AppComponent implements OnInit {
 
   location = {};
@@ -17,6 +27,8 @@ export class AppComponent implements OnInit {
 
   PARK_LAT:string = 'parkLAT';
   PARK_LON:string = 'parkLON';
+
+  map: mapboxgl.Map; // Mapbox GL Map object (Mapbox is ran outside angular zone, keep that in mind when binding events from this object)
 
   constructor() {
   }
@@ -32,6 +44,19 @@ export class AppComponent implements OnInit {
     if (this.parkLat!=0 && this.parkLon!=0) {
       this.renderMarker = true;
     }
+    Object.getOwnPropertyDescriptor(mapboxgl, "accessToken").set('pk..oYdvq_XT79XIefBeuDNFVg');
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v9',
+      zoom: 16,
+      center: [this.lon, this.lat]
+    });
+    //// Add Marker on longpress
+    this.map.on('contextMenu', (event) => {
+      //const coordinates = [event.lngLat.lng, event.lngLat.lat]
+      //const newMarker   = new GeoJson(coordinates, { message: this.message })
+      //this.mapService.createMarker(newMarker)
+    })    
   }
 
   updateLocation() {
